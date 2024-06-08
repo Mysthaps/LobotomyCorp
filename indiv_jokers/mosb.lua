dbfcdc3786b7b86052b8d7dd1e85e525b61968bb
MyseIf <helluser0@gmail.com>
2024-06-08 23:14:44 +0700

    mosb scaling var for cryptid

--- indiv_jokers/mosb.lua
+++ indiv_jokers/mosb.lua
@@ -1,6 +1,6 @@
 local joker = {
     name = "The Mountain of Smiling Bodies",
-    config = {extra = {chips = 0, mult = 0, chips_gain = 5, mult_gain = 2, destroyed = 0}}, rarity = 3, cost = 8,
+    config = {extra = {chips = 0, mult = 0, chips_gain = 5, mult_gain = 2, gain_scale = 1, destroyed = 0}}, rarity = 3, cost = 8,
     pos = {x = 7, y = 4}, 
     blueprint_compat = true, 
     eternal_compat = false,
@@ -13,7 +13,7 @@ local joker = {
             "Destroys all {C:attention}scored{} cards",
             "This Abnormality gains {C:chips}+#3#{} Chips and",
             "{C:mult}+#4#{} Mult when it destroys a card",
-            "Increase {C:chips}Chips{} and {C:mult}Mult{} gain by {C:attention}1{}",
+            "Increase {C:chips}Chips{} and {C:mult}Mult{} gain by {C:attention}#8#{}",
             "if {C:attention}5{} cards are destroyed at once",
             "{C:inactive}(Currently {C:chips}+#1#{C:inactive} Chips and {C:mult}+#2#{C:inactive} Mult)"
         }
@@ -71,8 +71,8 @@ joker.calculate = function(self, card, context)
                 end
 
                 if card.ability.extra.destroyed == 5 then
-                    card.ability.extra.chips_gain = card.ability.extra.chips_gain + 1
-                    card.ability.extra.mult_gain = card.ability.extra.mult_gain + 1
+                    card.ability.extra.chips_gain = card.ability.extra.chips_gain + card.ability.extra.gain_scale
+                    card.ability.extra.mult_gain = card.ability.extra.mult_gain + card.ability.extra.gain_scale
                 end
             return true
             end
@@ -113,7 +113,9 @@ end
 joker.generate_ui = function(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
     local vars = { card.ability.extra.chips, card.ability.extra.mult, 
                 card.ability.extra.chips_gain, card.ability.extra.mult_gain,
-                card:check_rounds(3), card:check_rounds(6), card:check_rounds(9) }
+                card:check_rounds(3), card:check_rounds(6), card:check_rounds(9),
+                card.ability.extra.gain_scale
+            }
     local desc_key = self.key
     if card:check_rounds(3) < 3 then
         desc_key = 'dis_'..desc_key..'_1'
